01edf811835b2d46748922cd3e4810746a9bd70b
donkeyfish1997 <donkeyfish1997@gamil.com>
2021-10-21 09:16:12 +0800

    v2

--- src/components/asideMenu.vue
+++ src/components/asideMenu.vue
@@ -2,6 +2,9 @@
 import { onMounted } from "@vue/runtime-core";
 import { mapGetters } from "vuex";
 export default {
+  data() {
+    return { menuOpen: false };
+  },
   computed: {
     currCity: {
       get() {
@@ -52,6 +55,9 @@ export default {
     },
   },
   methods: {
+    menuClick() {
+      this.menuOpen = !this.menuOpen;
+    },
     keywordHighlight(val) {
       return this.keywords
         ? val.replace(
@@ -69,53 +75,56 @@ export default {
 </script>
 
 <template>
-  <div class="aside-menu">
-    <div class="wraps">
-      <label>
-        縣市：<select v-model="currCity">
-          <option v-for="c in cityList" :key="c">{{ c }}</option>
-        </select>
-      </label>
-      <label>
-        行政區：<select v-model="currDistrict">
-          <option v-for="d in districtList" :key="d.id">{{ d.name }}</option>
-        </select>
-      </label>
+  <div :class="['aside-menu',{open:menuOpen}]">
+    <div @click="menuClick" class="openBtn"><p>></p></div>
+    <div class="container">
+      <div class="wraps">
+        <label>
+          縣市：<select v-model="currCity">
+            <option v-for="c in cityList" :key="c">{{ c }}</option>
+          </select>
+        </label>
+        <label>
+          行政區：<select v-model="currDistrict">
+            <option v-for="d in districtList" :key="d.id">{{ d.name }}</option>
+          </select>
+        </label>
+      </div>
+
+      <div class="wraps">
+        <label>
+          <i class="fas fa-search-location"></i> 關鍵字搜尋：
+          <input type="text" placeholder="請輸入關鍵字" v-model="keywords" />
+        </label>
+      </div>
+
+      <ul class="store-lists">
+        <li
+          class="store-info wraps"
+          v-for="s in filteredStores"
+          :key="s.id"
+          @click="$emit('triggerMarkerPopup', s.id)"
+        >
+          <h1 v-html="keywordHighlight(s.name)"></h1>
+          <div class="mask-info">
+            <i class="fas fa-head-side-mask"></i>
+            <span>大人口罩: {{ s.mask_adult }} 個</span>
+          </div>
+
+          <div class="mask-info">
+            <i class="fas fa-baby"></i>
+            <span>兒童口罩: {{ s.mask_child }} 個</span>
+          </div>
+
+          <div class="mask-info">最後更新時間: {{ s.updated }}</div>
+
+          <button class="btn-store-detail" @click="openInfoBox(s.id)">
+            <i class="fas fa-info-circle"></i>
+            看詳細資訊
+          </button>
+        </li>
+      </ul>
     </div>
-
-    <div class="wraps">
-      <label>
-        <i class="fas fa-search-location"></i> 關鍵字搜尋：
-        <input type="text" placeholder="請輸入關鍵字" v-model="keywords" />
-      </label>
-    </div>
-
-    <ul class="store-lists">
-      <li
-        class="store-info wraps"
-        v-for="s in filteredStores"
-        :key="s.id"
-        @click="$emit('triggerMarkerPopup', s.id)"
-      >
-        <h1 v-html="keywordHighlight(s.name)"></h1>
-        <div class="mask-info">
-          <i class="fas fa-head-side-mask"></i>
-          <span>大人口罩: {{ s.mask_adult }} 個</span>
-        </div>
-
-        <div class="mask-info">
-          <i class="fas fa-baby"></i>
-          <span>兒童口罩: {{ s.mask_child }} 個</span>
-        </div>
-
-        <div class="mask-info">最後更新時間: {{ s.updated }}</div>
-
-        <button class="btn-store-detail" @click="openInfoBox(s.id)">
-          <i class="fas fa-info-circle"></i>
-          看詳細資訊
-        </button>
-      </li>
-    </ul>
   </div>
 </template>
 
